c5515cceebe2984dce392886f2101cffdfc04842
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-08-08 21:08:12 +0300

    updat day 6

---
+++ Day7Lab/IOperator.cs
@@ -0,0 +1,16 @@
+﻿namespace Day7Lab
+{
+    public interface IOperator //CRUD Create , Update , Read(All , By Id) , Delete 
+    {
+        void Create();
+        void Delete();
+        void Update();
+        void GetAll();
+        void GetById(int id);
+
+        //public void GetByName(string name)
+        //{ 
+
+        //}
+    }
+}
---
+++ IO_Demo/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace IO_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            lbl = new Label();
+            label3 = new Label();
+            listBox2 = new ListBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            richTextBox1 = new RichTextBox();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 123);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(204, 344);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 63);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(166, 28);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Drive";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Folder";
+            // 
+            // lbl
+            // 
+            lbl.Location = new Point(184, -1);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(268, 92);
+            lbl.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(245, 100);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Files";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(245, 123);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(243, 344);
+            listBox2.TabIndex = 6;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(518, 122);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(350, 345);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(663, 87);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 8;
+            button1.Text = "Create";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(777, 87);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 9;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.Location = new Point(874, 123);
+            label4.Name = "label4";
+            label4.Size = new Size(263, 344);
+            label4.TabIndex = 10;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(1143, 163);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(248, 283);
+            richTextBox1.TabIndex = 11;
+            richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1143, 452);
+            button3.Name = "button3";
+            button3.Size = new Size(248, 29);
+            button3.TabIndex = 12;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1146, 123);
+            button4.Name = "button4";
+            button4.Size = new Size(245, 29);
+            button4.TabIndex = 13;
+            button4.Text = "Read";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(1433, 123);
+            button5.Name = "button5";
+            button5.Size = new Size(152, 29);
+            button5.TabIndex = 14;
+            button5.Text = "Open File Stream";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(1433, 172);
+            button6.Name = "button6";
+            button6.Size = new Size(152, 29);
+            button6.TabIndex = 15;
+            button6.Text = "Write File Stream";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1735, 494);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(richTextBox1);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(listBox2);
+            Controls.Add(label3);
+            Controls.Add(lbl);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "IO";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label lbl;
+        private Label label3;
+        private ListBox listBox2;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private RichTextBox richTextBox1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
