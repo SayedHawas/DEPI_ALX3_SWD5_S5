71b420fd03ad5dd1421a3518e1dc18550a88c9b3
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-09-30 22:14:42 +0300

    Update

---
+++ Day21WebApiLab/Migrations/20250930172213_IntitalMigration.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Day21WebApiLab.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TblDepartments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblDepartments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TblEmployees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    departmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblEmployees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TblEmployees_TblDepartments_departmentId",
+                        column: x => x.departmentId,
+                        principalTable: "TblDepartments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TblDepartments",
+                columns: new[] { "DepartmentId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "HR" },
+                    { 2, null, "Software" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TblEmployees_departmentId",
+                table: "TblEmployees",
+                column: "departmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TblEmployees");
+
+            migrationBuilder.DropTable(
+                name: "TblDepartments");
+        }
+    }
+}
---
+++ Day21WebApiLab/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day21WebApiLab.Models
+{
+    [Table("TblEmployees")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Must Enter Employee Name ")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Must Enter Job")]
+        [MaxLength(100)]
+        public string Job { get; set; }
+        [Required]
+        [Range(typeof(decimal), "0.00", "9999999.99")]
+        [RegularExpression(@"^\d+.?\d{0,2}$")]
+        public decimal Salary { get; set; }
+
+        // Navigation Property
+        [ForeignKey("department")]
+        //[ForeignKey(nameof(department))]
+        public int departmentId { get; set; }
+        public virtual Department department { get; set; }
+    }
+}
